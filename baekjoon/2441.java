98eefc97ebd6ab43abbe31fa39b5839e0d3f4f28
dongjun lee <ddongjunn@naver.com>
2022-01-17 23:27:51 +0900

    별 찍기 - 4

---
+++ baekjoon/2441.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            for(int x = 0; x < i; x++){
+                System.out.print(" ");
+            }
+            for(int j = N; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
