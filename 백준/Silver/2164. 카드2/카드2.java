c024570823e1f14afe74660d1c318c6906700205
dongjun lee <ddongjunn@naver.com>
2023-02-27 21:34:58 +0900

    [Silver IV] Title: 카드2, Time: 140 ms, Memory: 43228 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,19 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main{
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        LinkedList<Integer> queue = new LinkedList<>();
-        for(int i=1; i<=N; i++) queue.add(i);
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=N; i++) queue.offer(i);
 
         while(queue.size() != 1){
-            queue.pollFirst();
-            queue.add(queue.pollFirst());
+            queue.poll();
+            queue.add(queue.poll());
         }
-        System.out.println(queue.get(0));
+        System.out.println(queue.poll());
     }
 }
