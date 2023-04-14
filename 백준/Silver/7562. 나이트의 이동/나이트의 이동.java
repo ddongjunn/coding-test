914fd421cc037101c9fe8e4ed07a3a4b27478f22
dongjun lee <ddongjunn@naver.com>
2023-04-14 18:31:26 +0900

    [Silver I] Title: 나이트의 이동, Time: 256 ms, Memory: 63088 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,62 +1,56 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main{
-    static final int[] dx = {1, 2,  2,  1, -1, -2, -2, -1};
-    static final int[] dy = {2, 1, -1, -2, -2, -1,  1, 2,};
-    static int[][] board;
-    static Queue<Point> q;
-    public static void main(String[] args) throws Exception{
+public class Main {
+    static int[] dr = {-1, -2, -2, -1, 1, 2, 2, 1};
+    static int[] dc = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tastCase = Integer.parseInt(br.readLine());
-        for(int k=0; k<tastCase; k++){
-            int boardLen = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
-            board = new int[boardLen][boardLen];
-            q = new LinkedList<>();
-
-            StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            Point startPoint = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())); //현재위치
-
-            st = new StringTokenizer(br.readLine(), " ");
-            Point endPoint = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())); //목적지
-
-            int distance = bfs(startPoint, endPoint);
-            System.out.println(distance);
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++){
+            int n = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            int start_r = Integer.parseInt(st.nextToken());
+            int start_c = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int end_r = Integer.parseInt(st.nextToken());
+            int end_c = Integer.parseInt(st.nextToken());
+            bfs(n, new Node(start_r, start_c, 0), new int[] {end_r, end_c});
         }
     }
+    public static void bfs(int n, Node start, int[] end){
+        boolean[][] visited = new boolean[n][n];
 
-    public static int bfs(Point startPoint, Point endPoint){
-        q = new LinkedList<>();
-        q.add(startPoint);
+        Queue<Node> q = new LinkedList<>();
+        q.add(start);
+        visited[start.r][start.c] = true;
 
         while(!q.isEmpty()){
-            Point point = q.poll();
-            if(point.x == endPoint.x && point.y == endPoint.y) return board[point.x][point.y]; 
-
-            for(int dir=0; dir<dx.length; dir++){
-                int nx = point.x + dx[dir]; 
-                int ny = point.y + dy[dir];
-                if(nx < 0 || ny < 0 || nx >= board.length || ny >= board.length) continue;
-                if(board[nx][ny] > 0) continue;
-
-                q.add(new Point(nx, ny));
-                board[nx][ny] = board[point.x][point.y] + 1;
+            Node tmp = q.poll();
+            if (tmp.r == end[0] && tmp.c == end[1]){
+                System.out.println(tmp.cnt);
+                return;
+            }
+            for (int i = 0; i < 8; i++){
+                int newR = tmp.r + dr[i];
+                int newC = tmp.c + dc[i];
+                if (0 <= newR && newR < n && 0 <= newC && newC < n && !visited[newR][newC]){
+                    q.add(new Node(newR, newC, tmp.cnt + 1));
+                    visited[newR][newC] = true;
+                }
             }
         }
-        return -1;
     }
-}
+    public static class Node {
+        int r, c, cnt;
 
-class Point{
-    int x;
-    int y;
-
-    Point(int x, int y){
-        this.x = x;
-        this.y = y;
+        public Node(int r, int c, int cnt) {
+            this.r = r;
+            this.c = c;
+            this.cnt = cnt;
+        }
     }
 }
