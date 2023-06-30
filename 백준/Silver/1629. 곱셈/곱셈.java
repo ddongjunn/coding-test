2ab461cf720eeabcd1c5a6e781939acd7929ce21
dongjun lee <ddongjunn@naver.com>
2023-06-30 15:18:33 +0900

    [Silver I] Title: 곱셈, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        long answer = func(a, b, c);
+        System.out.println(answer);
+    }
+
+    public static long func(long a, long b, long m){
+        if(b == 1){
+            return a % m;
+        }
+        long val = func(a, b / 2, m);
+        val = val * val % m;
+        if(b % 2 == 0){
+            return val;
+        }
+        return val * a % m;
+    }
+}
