de2c6aed18cc7dac10263ec1623e9c22587fdfca
dongjun lee <ddongjunn@naver.com>
2023-08-10 11:08:34 +0900

    [Silver III] Title: N과 M (6), Time: 84 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+    static int N, M;
+    static int inArr[], outArr[];
+    static boolean[] isVisited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        inArr = new int[N];
+        outArr = new int[M];
+        isVisited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            inArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(inArr);
+        solution(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    static void solution(int at, int depth){
+        if(depth == M){
+            for(int i : outArr){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = at; i < N; i++){
+            if(!isVisited[i]){
+                isVisited[i] = true;
+                outArr[depth] = inArr[i];
+                solution(i, depth + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
+}
