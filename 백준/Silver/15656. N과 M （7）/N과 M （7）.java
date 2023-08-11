655cd76c56eb9ed0ee8d7abae25ec4f56faca02c
dongjun lee <ddongjunn@naver.com>
2023-08-11 17:19:18 +0900

    [Silver III] Title: N과 M (7), Time: 604 ms, Memory: 220024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+    static int N, M;
+    static int[] inArr, outArr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        inArr = new int[N];
+        outArr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            inArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(inArr);
+        solution(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    public static void solution(int start, int depth){
+        if(depth == M){
+            for(int i=0; i<M; i++){
+                sb.append(outArr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            outArr[depth] = inArr[i];
+            solution(i, depth + 1);
+        }
+    }
+}
