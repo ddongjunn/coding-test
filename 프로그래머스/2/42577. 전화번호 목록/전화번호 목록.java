2e8f9aaa847477d099d55b3430d13cfc0c956a58
dongjun lee <ddongjunn@naver.com>
2024-05-20 22:08:29 +0900

    [level 2] Title: 전화번호 목록, Time: 226.10 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,24 @@
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Arrays;
+
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = Stream.of(phone_book).collect(Collectors.toSet());
+        Set<String> set = new HashSet<>();
+        for(String phone : phone_book) {
+            set.add(phone);
+        }  
         
-        for (String phone : phone_book) {
-            for (int i = 1; i < phone.length(); i++) {
-                if (set.contains(phone.substring(0, i))) {
+        for(String phone : phone_book) {
+            for(int i = 0; i < phone.length(); i++) {
+                if(set.contains(phone.substring(0, i))) { 
                     return false;
                 }
             }
         }
+        
         return true;
     }
 }
