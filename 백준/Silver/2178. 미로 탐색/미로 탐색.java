248ccd0631d648ddcf66aedafc15144dfceae66a
dongjun lee <ddongjunn@naver.com>
2024-07-03 10:59:02 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12368 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,61 +1,70 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws Exception{
+public class Main {
+    static final int[] dx = {0, 1, 0, -1};
+    static final int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input," ");
-
-        int[] dx = {1,0,-1,0};
-        int[] dy = {0,1,0,-1};
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
+
         int[][] board = new int[n][m];
-        for(int i=0; i<n; i++){
-            String str = br.readLine();
-            for(int j=0; j<m; j++){
-                board[i][j] = str.charAt(j) - '0';
+        for(int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for(int j = 0; j < m; j++){
+                board[i][j] = line.charAt(j) - '0';
             }
         }
 
-        int[][] dist = new int[n][m];
-        for (int[] is : dist) {
-            Arrays.fill(is, -1);
-        }
+        boolean[][] visited = new boolean[n][m];
+        int[][] distance = new int[n][m];
+        Queue<Pos> q = new LinkedList<>();
+        
+        visited[0][0] = true;
+        distance[0][0] = 1;
+        q.offer(new Pos(0, 0));
 
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0, 0));
-        dist[0][0] = 1;
-        while(!q.isEmpty()){
-            Pair pair = q.poll();
-            for(int dir=0; dir<4; dir++){
-                int nx = pair.x + dx[dir];
-                int ny = pair.y + dy[dir];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if(dist[nx][ny] >= 0 || board[nx][ny] != 1) continue;
+        while(!q.isEmpty()) {
+            Pos pos = q.poll();
+            int x = pos.x;
+            int y = pos.y;
 
-                dist[nx][ny] = dist[pair.x][pair.y]+1;
-                q.add(new Pair(nx, ny));
+            for(int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+
+                if(visited[nx][ny] || board[nx][ny] == 0) {
+                    continue;
+                }
+
+                visited[nx][ny] = true;
+                distance[nx][ny] = distance[x][y] + 1;
+                q.offer(new Pos(nx, ny));
             }
         }
-        System.out.println(dist[n-1][m-1]);
+        System.out.println(distance[n - 1][m - 1]);
     }
 }
 
-class Pair{
+class Pos {
     int x;
     int y;
-    
-    Pair(int x, int y){
+    Pos(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
-
