81c6af4881836f436377cc6df0af04668949a249
dongjun lee <ddongjunn@naver.com>
2024-07-09 18:05:24 +0900

    [Gold III] Title: 불!, Time: 540 ms, Memory: 78460 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -5,83 +5,111 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
+    static int R, C;
+    static char[][] map;
+    static int[][] jihoon, fire;
+
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        int[] dx = {1,0,-1,0};
-        int[] dy = {0,1,0,-1};
-        int R = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        
-        Queue<Pair> fire = new LinkedList<>();
-        int[][] fireDist = new int[R][C];
-        for (int[] is : fireDist) {
-            Arrays.fill(is, -1);
+        map = new char[R][C];
+        jihoon = new int[R][C];
+        fire = new int[R][C];
+
+        for(int i = 0; i < R; i++){
+            Arrays.fill(jihoon[i], -1);
+            Arrays.fill(fire[i], -1);
         }
 
-        Queue<Pair> person = new LinkedList<>();
-        int[][] personDist = new int[R][C];
-        for (int[] is : personDist) {
-            Arrays.fill(is, -1);
-        }
+        Queue<int[]> qJ = new LinkedList<>();
+        Queue<int[]> qF = new LinkedList<>();
 
-        String[][] maze = new String[R][C];
-        for(int i=0; i<R; i++){
-            String input = br.readLine();
-            for(int j=0; j<C; j++){
-                String s = Character.toString(input.charAt(j));
-                maze[i][j] = s;
-                if(s.equals("F")){
-                    fire.add(new Pair(i, j));
-                    fireDist[i][j] = 0;
+        for(int i = 0; i < R; i++) {
+            String line = br.readLine();
+            for(int j = 0; j < C; j++) {
+                map[i][j] = line.charAt(j);
+
+                if(map[i][j] == 'J') {
+                    qJ.add(new int[]{i, j});
+                    jihoon[i][j] = 0;
                 }
-                if(s.equals("J")){ 
-                    person.add(new Pair(i, j));
-                    personDist[i][j] = 0;
+
+                if(map[i][j] == 'F') {
+                    qF.offer(new int[]{i, j});
+                    fire[i][j] = 0;
                 }
             }
         }
 
-        //불에 대한 BFS
-        while(!fire.isEmpty()){ 
-            Pair pair = fire.poll();
-            for(int dir=0; dir<4; dir++){
-                int nx = pair.x + dx[dir];
-                int ny = pair.y + dy[dir];
-                if(nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
-                if(maze[nx][ny].equals("#") || fireDist[nx][ny] >= 0) continue;
-                fireDist[nx][ny] = fireDist[pair.x][pair.y] + 1;
-                fire.add(new Pair(nx, ny));
+        findEscape(qJ, qF);
+    }
+
+    public static void findEscape(Queue<int[]> qJ, Queue<int[]> qF){
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        while(!qF.isEmpty()) {
+            int[] cur = qF.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for(int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if(isRange(nx, ny)) {
+                    continue;
+                }
+
+                if(map[nx][ny] == '#' || fire[nx][ny] >= 0) {
+                    continue;
+                }
+
+                qF.offer(new int[]{nx, ny});
+                fire[nx][ny] = fire[x][y] + 1;
             }
         }
 
-        //지훈이에 대한 BFS
-        while(!person.isEmpty()){
-            Pair pair = person.poll();
-            for(int dir=0; dir<4; dir++){
-                int nx = pair.x + dx[dir];
-                int ny = pair.y + dy[dir];
-                if(nx < 0 || ny < 0 || nx >= R || ny >= C){ //범위를 벗어났다는 것은 탈출성공. 큐에 거리순으로 들어가기 때문에 최초에 탈출한 시간
-                    System.out.println(personDist[pair.x][pair.y]+1);
+        while(!qJ.isEmpty()) {
+            int[] cur = qJ.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int dist = jihoon[x][y] + 1;
+
+            for(int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if(isRange(nx, ny)) {
+                    System.out.println(dist);
                     System.exit(0);
                 }
-                if(personDist[nx][ny] >= 0 || maze[nx][ny].equals("#")) continue;
-                if(fireDist[nx][ny] != -1 && fireDist[nx][ny] <= personDist[pair.x][pair.y]+1) continue;
-                personDist[nx][ny] = personDist[pair.x][pair.y] + 1;
-                person.add(new Pair(nx, ny));
+                
+                if(isFire(nx, ny, dist)) {
+                    continue;
+                }
+
+                if(map[nx][ny] == '#' || jihoon[nx][ny] >= 0) {
+                    continue;
+                }
+
+                qJ.offer(new int[]{nx, ny});
+                jihoon[nx][ny] = dist;
             }
         }
+
         System.out.println("IMPOSSIBLE");
     }
-}
-class Pair{
-    int x;
-    int y;
 
-    Pair(int x, int y){
-        this.x = x;
-        this.y = y;
+    public static boolean isRange(int x, int y) {
+        return x < 0 || x >= R || y < 0 || y >= C;
+    }
+    
+    public static boolean isFire(int x, int y, int dist) {
+        return fire[x][y] <= dist && fire[x][y] != -1;
     }
 }
