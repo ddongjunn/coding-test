bd8673d99a2b2e7252a52ac9fb84ab7052ff5f85
dongjun lee <ddongjunn@naver.com>
2024-07-13 23:41:15 +0900

    [level 1] Title: 옹알이 (2), Time: 1.01 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] babbling) {   
+       int answer = 0;
+       for(int i = 0; i < babbling.length; i++) {
+            if(babbling[i].contains("ayaaya") || babbling[i].contains("yeye") || babbling[i].contains("woowoo") || babbling[i].contains("mama")) {
+                continue;
+            }
+
+            babbling[i] = babbling[i].replace("aya", " ");
+            babbling[i] = babbling[i].replace("ye", " ");
+            babbling[i] = babbling[i].replace("woo", " ");
+            babbling[i] = babbling[i].replace("ma", " ");
+
+            if(babbling[i].trim().length()  == 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
