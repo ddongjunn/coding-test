e5189d6b8099fbc0712b57ce6faf32d674df27a6
dongjun lee <ddongjunn@naver.com>
2024-07-23 16:04:53 +0900

    [level 1] Title: 실패율, Time: 111.81 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,30 +1,29 @@
-import java.util.ArrayList;
+import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] currentStages = new int[N + 1];
-        int[] clearStages = new int[N + 1];
-
-        for (int i = 0; i < stages.length; i++) {
-            for (int j = 0; j < stages[i]; j++) {
-                clearStages[j] += 1;
+        int[] current = new int[N + 1];
+        int[] clear = new int[N + 1];
+        for(int i = 0; i < stages.length; i++) {
+            for(int j = 0; j < stages[i]; j++) {
+                clear[j]++;
             }
-            currentStages[stages[i] - 1] += 1;
+            current[stages[i] - 1]++;
         }
 
-        Map<Integer, Double> map = new HashMap<>();
-
-        for (int i = 0; i < N; i++) {
-            if (currentStages[i] == 0 || clearStages[i] == 0) {
+        Map<Integer, Double> map = new HashMap();
+        for(int i = 0; i < N; i++) {
+            if(current[i] == 0 || clear[i] == 0) {
                 map.put(i + 1, 0.0);
             } else {
-                map.put(i + 1, (double) currentStages[i] / (double) clearStages[i]);
+                map.put(i + 1, (double) current[i] / (double) clear[i]);
             }
         }
-
+        
         List<Integer> list = new ArrayList<>(map.keySet());
         list.sort((o1, o2) -> Double.compare(map.get(o2), map.get(o1)));
         
