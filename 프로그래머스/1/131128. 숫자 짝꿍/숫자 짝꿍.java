40a7d6c25ad3523ce9510276ed87a5f3138b52c0
dongjun lee <ddongjunn@naver.com>
2024-07-24 14:15:32 +0900

    [level 1] Title: 숫자 짝꿍, Time: 109.42 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String X, String Y) {
+        int[] arrayX = new int[10];
+        for(char x : X.toCharArray()) {
+            arrayX[x - '0']++;
+        }
+
+        int[] arrayY = new int[10];
+        for(char y : Y.toCharArray()) {
+            arrayY[y - '0']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 9; i >= 0; i--) {
+            int commonCount = Math.min(arrayX[i], arrayY[i]);
+
+            for(int j = 0; j < commonCount; j++) {
+                sb.append(i);
+            }
+        }
+
+        if(sb.length() == 0) {
+            return "-1";
+        }
+
+        if(sb.charAt(0) == '0')  {
+            return "0";
+        }
+
+        return sb.toString();
+    }
+}
